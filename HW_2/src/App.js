9f976d16d2a8af55c88d74585ea765bf88e2b70d
EvgenySverchkov <199816ab@gmail.com>
2019-08-21 09:28:55 +0300

    correction

--- HW_2/src/App.js
+++ HW_2/src/App.js
@@ -11,8 +11,14 @@ class App extends React.Component{
 		this.state = {isTable:false};
 	}
 	onSubmit(userId){
-		this.setState({isTable:true});
 		this.userId = userId;
+		if(!this.userId){
+			alert("You should enter userId to continue");
+			this.setState({isTable:false});
+		}else{
+			this.setState({isTable:true});
+		}
+
 	}
 	backHandler(){
 		this.setState({isTable:false});
@@ -21,13 +27,7 @@ class App extends React.Component{
 		const isTable = this.state.isTable;
 		let component = null;
 		if(isTable){
-			if(!this.userId){
-				alert("You should enter userId to continue");
-				component = <Profile header={this.onSubmit}/>;
-			}
-			else{
-				component = <UserInfoTable idUser={this.userId} backHandler ={this.backHandler}/>;
-			}
+			component = <UserInfoTable idUser={this.userId} backHandler ={this.backHandler}/>;
 		}
 		
 		else{
